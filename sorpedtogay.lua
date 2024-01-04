cbfedd9970e653eec589d52c2a026710dfdc3469
x2n12 <147017566+x2n12@users.noreply.github.com>
2024-01-04 22:26:27 +0700

    Create sorpedtogay.lua

---
+++ sorpedtogay.lua
@@ -0,0 +1,8 @@
+repeat wait() until game:IsLoaded();
+if game.PlaceId == 13772394625 then
+    if _G.KAITUNG_SCRIPT then
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/x2n12/SORPEDSUNGLUYHUB/main/sorped.lua'))()
+    elseif _G.NORMAL_BLADEBALL then 
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/x2n12/sorpednormalkub/main/eiei.lua'))()
+    end
+end
